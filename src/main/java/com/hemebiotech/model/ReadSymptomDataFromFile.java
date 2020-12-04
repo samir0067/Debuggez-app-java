f611f26d93fddf4a98c0b549eaa51c6e84d89b5a
samir0067 <arrahmani.samir@gmail.com>
2020-12-04 16:21:34 +0100

    Cleaning comments unnecessary

--- src/main/java/com/hemebiotech/model/ReadSymptomDataFromFile.java
+++ src/main/java/com/hemebiotech/model/ReadSymptomDataFromFile.java
@@ -6,18 +6,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Simple brute force implementation
- *
- */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
+
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
 	}
