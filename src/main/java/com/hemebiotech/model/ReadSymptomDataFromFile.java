8649fc2b6b633ec6cd8a083bcc06a7219b701eca
samir <42518117+samir0067@users.noreply.github.com>
2020-12-04 16:24:22 +0100

    Feat clean (#2)
    * Restructuring architecture
    * Cleaning comments unnecessary

--- src/main/java/com/hemebiotech/model/ReadSymptomDataFromFile.java
+++ src/main/java/com/hemebiotech/model/ReadSymptomDataFromFile.java
@@ -1,4 +1,4 @@
-package com.hemebiotech.analytics;
+package com.hemebiotech.model;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -6,18 +6,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Simple brute force implementation
- *
- */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
+
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
 	}
