3d9f00070f82b403b701fae6455dce14a1ca5e6d
samir <42518117+samir0067@users.noreply.github.com>
2020-12-11 10:19:55 +0100

    Revert "Feature clean (#3)"
    This reverts commit c5696afd3089e9691a9bf6885de5b26545e6a417.

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,4 +1,4 @@
-package com.hemebiotech.model;
+package com.hemebiotech.analytics;
 
 import java.util.List;
 
@@ -6,12 +6,15 @@ import java.util.List;
  * Anything that will read symptom data from a source
  * The important part is, the return value from the operation, which is a list of strings,
  * that may contain many duplications
+ * 
  * The implementation does not need to order the list
+ * 
  */
-public interface SymptomReader {
+public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
+	 * 
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-	List<String> getSymptoms(String filepath);
+	List<String> GetSymptoms ();
 }
