07d78bdd29f9a156f8c6327230f194b37b4e7460
samir0067 <arrahmani.samir@gmail.com>
2020-12-11 22:44:21 +0100

    wip

--- src/main/java/com/hemebiotech/business/SymptomsBusiness.java
+++ src/main/java/com/hemebiotech/business/SymptomsBusiness.java
@@ -2,7 +2,6 @@ package com.hemebiotech.business;
 
 import com.hemebiotech.helper.SymptomsUtils;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -24,17 +23,13 @@ public class SymptomsBusiness {
 
     /**
      * obtain and write down the onset of symptoms
-     *
-     * @param symptomReader
-     * @throws IOException
      */
-    public void getOccurences() throws IOException {
+    public void getOccurences() {
         this.symptomsList = this.symptomReader.getSymptoms("symptoms.txt");
 
         Map<String, Integer> occurrenceMap = SymptomsUtils.convertToOccurrenceMap(symptomsList);
 
         this.occurrenceSymptomList = SymptomsUtils.occurenceMapToList(occurrenceMap);
-
     }
 
     public void writeOccurences(String fileName) {
